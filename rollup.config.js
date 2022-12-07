db6d247782faeb6a37e58d7b38f4fc3bcf0ce75d
literate <gaekoon@gmail.com>
2022-12-08 07:17:58 +0900

    add: config

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import { babel } from "@rollup/plugin-babel";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import postcss from "rollup-plugin-postcss";
+import typescript from "rollup-plugin-typescript2";
+import terser from "@rollup/plugin-terser";
+
+const extensions = ["js", "jsx", "ts", "tsx", "mjs"];
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const paths = {
+  input: resolve(__dirname, "./src/components/index.ts"),
+  file: resolve(__dirname, "./dist/bundle.js"),
+};
+
+export default {
+  input: paths.input,
+  output: {
+    file: paths.file,
+    format: "es",
+    sourcemap: true,
+  },
+  plugins: [
+    postcss({
+      config: {
+        path: "./postcss.config.cjs",
+      },
+      extensions: [".css"],
+      minimize: true,
+      use: ["sass"],
+    }),
+    commonjs({ include: "node_modules/**" }),
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**",
+      extensions,
+    }),
+    typescript({ useTsconfigDeclarationDir: true }),
+    nodeResolve({ extensions }),
+  ],
+  external: ["react", "react-dom", "typescript"],
+};
